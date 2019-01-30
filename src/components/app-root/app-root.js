343ffdda188d5fa35bff12486de5ef620dbd360e
m.partola <mpartola@ubuntu.ubuntu-domain>
2019-01-30 17:30:50 +0300

    initialize

---
+++ src/components/app-root/app-root.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import WordTranslationTrain from '../trains/word-translation/word-translation-train';
+
+class AppRoot extends Component {
+  render() {
+    return (
+      <WordTranslationTrain />
+    );
+  }
+}
+
+export default AppRoot;
---
+++ src/components/trains/word-translation/word-translation-train.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class WordTranslationTrain extends Component {
+  render() {
+    return (
+      <div className="word-translation-train">
+        <div className="word-translation-train__from-layout">
+          Some word
+        </div>
+        <div className="word-translation-train__options-layout">
+          Some option
+        </div>
+      </div>
+    );
+  }
+}
+
+export default WordTranslationTrain;
